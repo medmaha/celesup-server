3d3f2b5129b677be3bfacccf9619bd72c064f77f
= <=>
2023-02-01 18:07:51 +0000

    still on working on cookies for deployment ..

--- api/library/cookies.py
+++ api/library/cookies.py
@@ -3,15 +3,20 @@ from rest_framework.response import Response
 from datetime import datetime, timedelta
 import os
 
+from django.conf import settings
+
 
 class CSCookie:
     def __init__(self, request=None, response=None) -> None:
         self.response: Response = response
         self.request: HttpRequest = request
+        SECURE: bool = settings.SESSION_COOKIE_SECURE
+        SAMESITE = "None" if SECURE else "Lax"
+
         self.__default = {
             "path": str("/"),
-            "secure": bool(int(os.environ.get("SESSION_COOKIE_SAMESITE"))),
-            "samesite": os.environ.get("SESSION_COOKIE_SAMESITE"),
+            "secure": SECURE,
+            "samesite": SAMESITE,
         }
 
     def set(self, key: str, value: str | int, **kwargs) -> None:
